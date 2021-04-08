3d2e22d6c751cb38eac016e15d0dc6ff4a753dd0
José Manuel <josemanuel@baintex.com>
2021-04-08 14:29:25 +0200

    Eliminada api key introducida y corregido el espaciado en README

--- app/src/main/java/com/baintex/everousample/Constants.java
+++ app/src/main/java/com/baintex/everousample/Constants.java
@@ -2,5 +2,5 @@ package com.baintex.everousample;
 
 public class Constants {
 
-    public static final String API_KEY = "zi5S4aoed9T44ucvQrWuhNYrosg2jPe6fw3SRA3mXTA";
+    public static final String API_KEY = "REPLACE_API_KEY";
 }
